b782539112e4aca02786f8f5b11c966f9dab2269
Yunoya <sonrasamon15725@gmail.com>
2022-11-02 19:22:08 +0700

    add pic

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
+import { getFirestore, collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ2R2VsEAtez0rww3CC43KErhigGgKH00",
+  authDomain: "relationship-34840.firebaseapp.com",
+  projectId: "relationship-34840",
+  storageBucket: "relationship-34840.appspot.com",
+  messagingSenderId: "215001641076",
+  appId: "1:215001641076:web:ad41b94b8a52a7af09a121",
+  measurementId: "G-PWRVYSQLXB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app)
+const form = document.getElementById("addForm")
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  addDoc(collection(db,'user'),{
+    one:form.one.value,
+    two:form.two.value,
+    three:form.three.value,
+    four:form.four.value,
+    five:form.five.value,
+    six:form.six.value
+  })
+})
