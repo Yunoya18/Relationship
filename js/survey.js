739e5d8a512c80cf60ef7ce7f69ae0210a3f0f19
Yunoya <sonrasamon15725@gmail.com>
2022-11-29 23:34:22 +0700

    add survey + edit bug

--- js/survey.js
+++ js/survey.js
@@ -20,32 +20,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app)
-const form = document.getElementById("addForm")
+const survey = document.getElementById("survey")
 
-form.addEventListener('submit',(e)=>{
+survey.addEventListener('submit',(e)=>{
   e.preventDefault()
-  addDoc(collection(db,'user'),{
-    one:form.one.value,
-    two:form.two.value,
-    three:form.three.value,
-    four:form.four.value,
-    five:form.five.value,
-    six:form.six.value
+  addDoc(collection(db,'survey'),{
+    one:survey.s_one.value,
+    two:survey.s_two.value,
+    three:survey.s_three.value,
+    four:survey.s_four.value,
+    five:survey.s_five.value,
+    six:survey.s_six.value
   })
 })
-
-document.getElementById("addForm").onsubmit=function() {
-    let q1 = parseInt(document.querySelector('input[name = "one"]:checked').value);
-    let q2 = parseInt(document.querySelector('input[name = "two"]:checked').value);
-    let q3 = parseInt(document.querySelector('input[name = "three"]:checked').value);
-    let q4 = parseInt(document.querySelector('input[name = "four"]:checked').value);
-    let q5 = parseInt(document.querySelector('input[name = "five"]:checked').value);
-    let q6 = parseInt(document.querySelector('input[name = "six"]:checked').value);
-        
-    let result = q1 + q2 + q3 + q4 + q5 + q6;
-        
-    document.getElementById("score").innerHTML = result;
-
-    return false; // required to not refresh the page; just leave this here
-} //this ends the submit function
-        
